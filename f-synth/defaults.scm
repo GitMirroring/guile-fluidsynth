3229426526b21aa3649471a3a7579f3fa9d9050e
David Pirotte <david@altosw.be>
2025-10-30 19:27:54 -0300

    Adding (customizable) gain and soundfont (default) values
    * f-synth/defaults.scm: New gain and soundfont (default) values, in the
      stow, using 'gain and 'soundfont as their entry name).
    
    * Makefile.am: Updated fot both this module and the previously added
      stow utilisty module.

---
+++ f-synth/defaults.scm
@@ -0,0 +1,35 @@
+;; -*- mode: scheme; coding: utf-8 -*-
+
+;;;;
+;;;; Copyright (C) 2025
+;;;; David Pirotte (david at altosw dot be)
+
+;;;; This file is part of Guile-Fluidsynth
+
+;;;; Guile-Fluidsynth is free software; you can redistribute it and/or
+;;;; modify it under the terms of the GNU Lesser General Public License
+;;;; as published by the Free Software Foundation; either version 2.1 of
+;;;; the License, or (at your option) any later version.
+
+;;;; Guile-Fluidsynth is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;;; Lesser General Public License for more details.
+
+;;;; You should have received a copy of the GNU Lesser General Public
+;;;; License along with Guile-Fluidsynth.  If not, see
+;;;; <https://www.gnu.org/licenses/>.
+;;;;
+
+;;; Commentary:
+
+;;; Code:
+
+
+(define-module (f-synth defaults)
+  #:use-module (f-synth support stow))
+
+
+(eval-when (expand load eval)
+  (stow-set! 'gain 0.4)
+  (stow-set! 'soundfont "/usr/share/sounds/sf2/FluidR3_GM.sf2"))
